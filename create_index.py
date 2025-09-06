af17537e423635081be57d9ac1fcf345a867afc8
Edwin <emarshall85@gmail.com>
2025-09-05 21:33:00 -0700

    Implemented separate indexes per tag

--- create_index.py
+++ create_index.py
@@ -1,57 +1,75 @@
 import csv
 from string import Template
-import sys
+import os
 
-index = {}
-post_template = Template("[$title](https://fetlife.com/$author/posts/$post")
-post_template = Template(
+POST_TEMPLATE = Template(
     "- [$title](https://fetlife.com/users/$user_id/posts/$post_id)\n"
 )
-with open("./backup/posts.csv", "r", newline="") as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        if row["deleted_at"] or not row["category"]:
-            continue
 
-        body = row["body"]
-        tags = []
-        for i, line in enumerate(body.split("\n")[::-1]):
-            if line.startswith("tags:"):
-                tags = [
-                    tag.strip()
-                    for tag in line.strip("tags:").split(",")
-                    if tag.strip().startswith("#")
-                ]
-                break
+def main():
+    index = {}
+    with open("./backup/posts.csv", "r", newline="") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if any([
+                row["deleted_at"],
+                not row["category"],
+                row["title"].startswith("#"),
+                "index" in row["title"].lower()
+            ]):
+                continue
 
-        post = {
-            "id": row["id"],
-            "user_id": row["user_id"],
-            "title": row["title"],
-            "created_at": row["created_at"],
-            "last_updated": row["updated_at"],
-            "body": row["body"] 
-        }
+            body = row["body"]
+            tags = []
+            for i, line in enumerate(body.split("\n")[::-1]):
+                if line.startswith("tags:"):
+                    tags = [
+                        tag.strip()
+                        for tag in line.strip("tags:").split(",")
+                        if tag.strip().startswith("#")
+                    ]
+                    break
 
-        for tag in tags:
-            if tag not in index:
-                index[tag] = []
+            post = {
+                "id": row["id"],
+                "user_id": row["user_id"],
+                "title": row["title"],
+                "created_at": row["created_at"],
+                "last_updated": row["updated_at"],
+                "body": row["body"] 
+            }
 
-            index[tag].append(post)
+            for tag in tags:
+                if tag not in index:
+                    index[tag] = []
 
-        if not tags:
-            category = row["category"]
+                index[tag].append(post)
+
+            category = "#" + row["category"].lower().replace(" ", "-")
             if category not in index:
                 index[category] = []
 
             index[category].append(post)
 
-with open("index.md", "w") as mdfile:
     for tag, posts in index.items():
-        mdfile.write(f"## {tag}\n")
-        for post in sorted(posts, key=lambda p: p["title"]):
-            mdfile.write(post_template.substitute(
-                title=post["title"],
+        with open(f"output/{tag.lstrip('#')}.md", "w") as mdfile:
+            mdfile.write(f"## {tag}\n")
+            for post in sorted(posts, key=lambda p: p["title"]):
+                mdfile.write(POST_TEMPLATE.substitute(
+                    title=post["title"],
+                    user_id=post["user_id"],
+                    post_id=post["id"]
+                ))
+
+    with open(f"output/index.md", "w") as mdfile:
+        mdfile.write("## Index\n\n")
+        for tag in sorted(index.keys()):
+            mdfile.write(POST_TEMPLATE.substitute(
+                title=tag,
                 user_id=post["user_id"],
-                post_id=post["id"]
+                post_id="REPLACE_ME"
             ))
+
+
+if __name__ == "__main__":
+    main()
