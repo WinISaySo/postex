d5fb03e7d985b9e5ab743cd36aaffa7b2e198ecf
Edwin <emarshall85@gmail.com>
2025-09-05 20:45:29 -0700

    Got rudimentary tag parsing to work

--- create_index.py
+++ create_index.py
@@ -1,5 +1,6 @@
 import csv
 from string import Template
+import sys
 
 index = {}
 post_template = Template("[$title](https://fetlife.com/$author/posts/$post")
@@ -12,21 +13,42 @@ with open("./backup/posts.csv", "r", newline="") as csvfile:
         if row["deleted_at"] or not row["category"]:
             continue
 
-        if row["category"] not in index:
-              index[row["category"]] = []
+        body = row["body"]
+        tags = []
+        for i, line in enumerate(body.split("\n")[::-1]):
+            if line.startswith("tags:"):
+                tags = [
+                    tag.strip()
+                    for tag in line.strip("tags:").split(",")
+                    if tag.strip().startswith("#")
+                ]
+                break
 
-        index[row["category"]].append({
+        post = {
             "id": row["id"],
             "user_id": row["user_id"],
             "title": row["title"],
             "created_at": row["created_at"],
             "last_updated": row["updated_at"],
             "body": row["body"] 
-        })
+        }
+
+        for tag in tags:
+            if tag not in index:
+                index[tag] = []
+
+            index[tag].append(post)
+
+        if not tags:
+            category = row["category"]
+            if category not in index:
+                index[category] = []
+
+            index[category].append(post)
 
 with open("index.md", "w") as mdfile:
-    for category, posts in index.items():
-        mdfile.write(f"## {category}\n")
+    for tag, posts in index.items():
+        mdfile.write(f"## {tag}\n")
         for post in sorted(posts, key=lambda p: p["title"]):
             mdfile.write(post_template.substitute(
                 title=post["title"],
